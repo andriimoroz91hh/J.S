734166c579d42f16f9c1fd7bdc635c9b7ec94138
andriimoroz91hh <andriimoroz91hh>
2022-02-02 18:07:50 +0200

    k/w_1

---
+++ klass/klass.w.1/main.js
@@ -0,0 +1,49 @@
+let result = [100+25+50+75+20+40+70+5+5+10];
+console.log(result);
+
+let book1 = {
+        b1:  `Alice's Adventures in Wonderland`,
+        peg: 192,
+        ge: `Adventures`
+    };
+
+let book2 =
+    {
+        b2: `The Adventures of Tom Sawyer`,
+        peg: 250,
+        ge: `Adventures`,
+        aut: `Mark Twain`
+    };
+
+let books = [
+    {
+        b1:  `Alice's Adventures in Wonderland`,
+        peg: 192,
+        ge: `Adventures`
+    },
+
+    {
+        b2: `The Adventures of Tom Sawyer`,
+        peg: 250,
+        ge: `Adventures`,
+        aut: `Mark Twain`
+    }
+    ];
+
+console.log(books,0,1);
+
+let height = 23;
+let width = 10;
+let S = 2 * (height + width);
+console.log(S);
+
+let heightC = 10;
+let dC = 4;
+let PI= 3.14;
+let V = PI * heightC * Math.pow(dC/2,2);
+console.log(V);
+
+let n = 3;
+let m = 4;
+let k = Math.sqrt(Math.pow(3,2) + Math.pow(4,2));
+console.log(k);
